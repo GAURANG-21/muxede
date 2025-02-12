a3adf2159ed91d590c69d96fdedb10db1f76c1a5
GAURANG-21 <gaurang2621@gmail.com>
2025-02-12 09:07:18 +0530

    fixes: cors fix

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,4 +1,3 @@
-import NextCors from 'nextjs-cors';
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
@@ -6,20 +5,16 @@ import Mux from "@mux/mux-node";
 
 const mux = new Mux();
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  await NextCors(req, {
-    methods: ['POST'],
-    origin: ['https://muxede-v6pd-git-master-gaurang-21s-projects.vercel.app', 'http://localhost:3000'], // Add your allowed origins here
-    optionsSuccessStatus: 200,
-  });
 
   const upload = await mux.video.uploads.create({
-    cors_origin: req.headers.get('origin') || '', // Set to the actual origin of the request
+    cors_origin: "*",
     new_asset_settings: {
       playback_policy: ["public", "signed"],
       passthrough: JSON.stringify({ userId: session.user?.id }),
